785dc6d7d25476f98255ab5d7fa734ef3d8858e4
jungtop <tenzintopjor27@gmail.com>
2023-01-31 06:42:11 +0000

    created release

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from image_downloader import BDRCImageDownloader
+from image_downloader import BDRCImageDownloader,_mkdir,DATA_DIR,BASE_PATH
 import github_utils
 from pathlib import Path
-
-def _mkdir(path):
-    if path.is_dir():
-        return path
-    path.mkdir(exist_ok=True, parents=True)
-    return path
-
-
-DATA_DIR = './data'
-BASE_PATH = _mkdir(Path.home() / ".openpecha")
-PECHAS_PATH = _mkdir(BASE_PATH / "pechas")
+import os
 
 
 
-def download():
-    downloader = BDRCImageDownloader(bdrc_scan_id="W26071", output_dir=Path(DATA_DIR),output_view = "zip")
-    img_dir = downloader.download()
-    # img_dir = Path('./data/W1KG26108')
+def download_image(work_id):
+    downloader = BDRCImageDownloader(bdrc_scan_id=work_id, output_dir=Path(DATA_DIR),output_view = "zip")
+    downloader.download()
 
 def get_readme(work_id):
     work_id = f"|Work Id | {work_id}"
@@ -27,7 +16,7 @@ def get_readme(work_id):
     return readme
 
 def publish_repo(repo_name, asset_paths=None):
-    token = "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"
+    token = ""
     read_me = get_readme(repo_name)
     local_repo = _mkdir(BASE_PATH / repo_name)
     Path(local_repo / "readme.md").write_text(read_me)
@@ -38,7 +27,6 @@ def publish_repo(repo_name, asset_paths=None):
         layers=[],
         token=token
        )
-    #res = github_utils.create_github_repo(path=repo_name,org_name=org_name,token=token)
     if asset_paths:
         github_utils.create_release(
             repo_name,
@@ -47,10 +35,17 @@ def publish_repo(repo_name, asset_paths=None):
             token=token
         )
 
+def main():
+    work_ids = Path("work_ids.txt").read_text().splitlines()
+    for work_id in work_ids:
+        download_image(work_id)
+        zip_file = Path(f"{DATA_DIR}/zip/{work_id}.zip")
+        publish_repo(work_id,asset_paths=[zip_file])
+        break
+
+
 if __name__ == "__main__":
-    assets = [Path("W26071.zip")]
-    publish_repo("W26071",asset_paths=assets)
-    
+    main()   
 
 
     
